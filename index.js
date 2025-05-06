24c54ddbe7349a8747968efe7fd04d69b9756320
Leandra Wyss <127302017+leandraaaaaaa@users.noreply.github.com>
2025-05-06 12:45:17 +0000

    alle seiten erstellt

--- index.js
+++ index.js
@@ -13,6 +13,18 @@ app.get("/", async function (req, res) {
   res.render("start");
 });
 
+app.get("/menu", async function (req, res) {
+  res.render("menu");
+});
+
+app.get("/ueberuns", async function (req, res) {
+  res.render("ueberuns");
+});
+
+app.get("/kontakt", async function (req, res) {
+  res.render("kontakt");
+});
+
 /* Wichtig! Diese Zeilen müssen immer am Schluss der Website stehen! */
 app.listen(3010, () => {
   console.log(`Example app listening at http://localhost:3010`);
